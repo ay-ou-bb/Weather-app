9c9277f2a6ba27299d2c0d60fb5d3658fdc57ac7
ay-ou-bb <ayoubbo3333@gmail.com>
2026-04-17 12:06:40 +0100

    change the content of this project

---
+++ src/App1.jsx
@@ -0,0 +1,169 @@
+import {useEffect, useState} from "react"
+// material ui
+import { createTheme,ThemeProvider } from "@mui/material/styles";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import CloudIcon from '@mui/icons-material/Cloud';
+import Button from '@mui/material/Button';
+
+// external libraries
+import axios from "axios";
+import moment from "moment/min/moment-with-locales"
+
+import { useTranslation } from 'react-i18next';
+
+moment.locale("ar");
+
+
+const theme =createTheme({
+  typography:{
+    fontFamily:["IBM"]
+  }
+})
+
+let cancelAxios =null
+
+function App1() {
+
+  const { t, i18n } = useTranslation();
+
+  // ===== STATES =====//
+  const [dateAndTime,setDateAndTime]=useState("")
+  const [temp , setTemp] = useState(
+    {
+      number:null,
+      description:"",
+      min:null,
+      max: null,
+      icon:null,
+    })
+  const [locale, setLocal] = useState("ar")
+
+    // ======== EVENT HANDLERS =====//
+
+    function handleLanguageClick(){
+      if(locale == "en"){
+        setLocal("ar")
+        i18n.changeLanguage("ar")
+        moment.locale("ar");
+
+      }else{
+        setLocal("en")
+        i18n.changeLanguage("en")
+        moment.locale("en");
+      }
+      setDateAndTime(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+    } 
+
+    useEffect(()=>{
+      i18n.changeLanguage(locale)
+    },[])
+
+  useEffect(()=>{
+
+    setDateAndTime(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+    axios.get('https://api.openweathermap.org/data/2.5/weather?lat=34.0084&lon=6.8539&appid=641ed9e18a9a0933c63051ca7f2e8e8a',
+    {
+      cancelToken:new axios.CancelToken((c)=>{
+        cancelAxios = c
+      })
+    }
+    )
+      .then(function (response) {
+        // handle success
+        const responseTemp = Math.round(response.data.main.temp -272.15)
+        const min = Math.round(response.data.main.temp_min -272.15)
+        const max = Math.round(response.data.main.temp_max -272.15)
+        const description = response.data.weather[0].description
+        const responseIcon = response.data.weather[0].icon
+
+        setTemp({number:responseTemp,min:min,max:max,description:description,icon:`http://openweathermap.org/img/wn/${responseIcon}.png`})        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+
+      return ()=>{
+        cancelAxios();
+      }
+
+      
+  },[])
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          {/* card */}
+          <div 
+            dir={locale=="ar"?"rtl":"ltr"}
+            style={{
+              width:"100%",
+              background:"rgb(28 52 91 / 36%)",
+              borderRadius:"15px",
+              padding :"10px", 
+              boxShadow:"0px 11px 1px rgba(0,0,0,0.05)"}}
+            >
+            {/* city & time */}
+              <div style={{display:"flex", alignItems:"end" , justifyContent: "start"}} dir={locale=="ar"?"rtl":"ltr"}>
+                <Typography variant="h2" style={{marginRight:"10px",fontWeight:"600"}}>
+                  {t("Rabat")}
+                </Typography>
+                <Typography variant="h6" style={{marginRight:"15px"}}>
+                  {dateAndTime}
+                </Typography>
+              </div>
+            {/*=== city & time ===*/}
+              <hr/>
+            {/* container temp & degre */}
+              <div style={{display:"flex" ,justifyContent:"space-around"}}>
+                {/* temp & description */}
+                <div>
+                  <div style={{display:"flex" ,justifyContent:"center", alignItems:"center"}}>
+                    <Typography variant="h1" style={{textAlign:"right"}}>
+                      {temp.number}
+                    </Typography>
+                    <img src={temp.icon}/>
+                  </div>
+                  <Typography variant="h6" style={{textAlign:"right"}}>
+                    {t(temp.description)}
+                  </Typography>
+                  <div style={{display:"flex",justifyContent:"space-between", alignItems:"center"}}>
+                    <h5>{t("min")}:{temp.min}</h5>
+                    <h5 style={{margin:"0 5px"}}> | </h5>
+                    <h5>{t("max")}:{temp.max}</h5>
+                  </div>
+                </div>
+                {/*=== temp & description === */}
+
+                {/* cloud */}
+                <div>
+                  <CloudIcon style={{fontSize:"200px"}}/>
+                </div>
+                {/*=== cloud ===*/}
+              </div>
+            {/*=== container temp & degre === */}
+
+            
+        </div>
+        {/*=== card ===*/}
+
+        {/* translation container */}
+        <div dir={locale=="ar"?"rtl":"ltr"} style={{width:"100%",display:"flex",justifyContent:"end",marginTop:"20px"}}>
+          <Button
+           variant="text" 
+           style={{color:"white"}} 
+           onClick={handleLanguageClick}>{locale=="en" ? "Arabic":"انجليزي"}
+          </Button>
+        </div>
+        {/* translation container */}
+        </Container>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App1
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,15 +1,137 @@
-import Typography from '@mui/material/Typography';
+import {useEffect, useState} from "react"
+import Card from '@mui/material/Card';
+import AirIcon from '@mui/icons-material/Air';
+import OpacityIcon from '@mui/icons-material/Opacity';
 
-export default function Test() {
+// external libraries
+import axios from "axios";
+import moment from "moment/min/moment-with-locales"
+
+import { useTranslation } from 'react-i18next';
+moment.locale("ar");
+
+export default function Weather() {
+
+   const { t, i18n } = useTranslation();
+   const apiKey = import.meta.env.VITE_API_KEY
+   const baseUrl = import.meta.env.VITE_BASE_URL
+
+  // ===== STATES =====//
+  const [dateAndTime,setDateAndTime]=useState("")
+  const [temp , setTemp] = useState(
+    {
+      number:null,
+      description:"",
+      min:null,
+      max: null,
+      feelsLike:null,
+      icon:null,
+      humidity:null,
+      windSpeed:null,
+    })
+  const [locale, setLocal] = useState("ar")
+
+  function handleLanguageClick(){
+      if(locale == "en"){
+        setLocal("ar")
+        i18n.changeLanguage("ar")
+        moment.locale("ar");
+
+      }else{
+        setLocal("en")
+        i18n.changeLanguage("en")
+        moment.locale("en");
+      }
+      setDateAndTime(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+    } 
+
+    useEffect(()=>{
+      i18n.changeLanguage(locale)
+    },[])
+
+    useEffect(()=>{
+
+    setDateAndTime(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+    axios.get(`${baseUrl}${apiKey}`,
+    {
+
+      cancelToken:new axios.CancelToken((c)=>{
+        cancelAxios = c
+      })
+    }
+    )
+      .then(function (response) {
+        // handle success
+        const responseTemp = Math.round(response.data.main.temp -272.15)
+        const min = Math.round(response.data.main.temp_min -272.15)
+        const max = Math.round(response.data.main.temp_max -272.15)
+        const feelsLike = Math.round(response.data.main.feels_like -272.15)
+        const description = response.data.weather[0].description
+        const responseIcon = response.data.weather[0].icon
+        const windSpeed = response.data.wind.speed
+        const humidity = response.data.main.humidity 
+        humidity
+
+        setTemp({
+          number:responseTemp,
+          min:min,
+          max:max,
+          description:description,
+          feelsLike:feelsLike,
+          icon:`http://openweathermap.org/img/wn/${responseIcon}.png`,
+          humidity:humidity,
+          windSpeed:windSpeed,   
+        })
+            
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+
+      return ()=>{
+        cancelAxios();
+      }
+
+      
+  },[])
+
+  let cancelAxios =null
+  
+
   return (
-
     <>
-    {/* CARD */}
-     <div>
-     
+       <div className="p-8 bg-gradient-to-br from-blue-500 to-purple-600 text-white border-none shadow-2xl rounded-lg">
+         <div className="flex flex-col items-center">
+            <h2 className="text-3xl mb-2">AL Argoub, Morocco</h2>
+            <p className="text-sm opacity-90 mb-2">{new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
+            <div className="flex items-center justify-center mb-6">
+              <img className="h-48" src={temp.icon}/>
+            </div>
+            <div className="text-6xl mb-2">{temp.number}°C</div>
+            <p className="text-xl mb-6">{temp.description}</p>
+            <p className="text-sm opacity-90 mb-8">Feels like {temp.feelsLike}°C</p>
 
-     </div>
-     {/* ==CARD== */}
+            <div className="grid grid-cols-2 gap-8 w-full max-w-xs">
+          <div className="flex items-center gap-3">
+            <OpacityIcon className="w-6 h-6" />
+            <div>
+              <p className="text-sm opacity-80">Humidity</p>
+              <p className="text-xl">{temp.humidity}%</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <AirIcon className="w-6 h-6"/>
+            <div>
+              <p className="text-sm opacity-80">Wind Speed</p>
+              <p className="text-xl">{temp.windSpeed} km/h</p>
+            </div>
+          </div>
+        </div>
+        </div>
+       </div>
     </>
   )
 }
